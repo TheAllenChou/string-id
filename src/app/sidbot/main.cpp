fe5602466e1d32386a02a6d6923bcd5d0fb82c84
Allen Chou <cjcat2266@gmail.com>
2016-09-03 09:59:18 -0700

    Change sidbot usage to local database terminal.

--- src/app/sidbot/main.cpp
+++ src/app/sidbot/main.cpp
@@ -6,121 +6,160 @@
  */
 /******************************************************************************/
 
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+
 #include "sid/sid.h"
 #include "siddb/siddb.h"
 
 void PrintUsage()
 {
   std::cout << "USAGE:\n"
-               "  - siddb --list\n"
-               "    Lists all SID-string pairs in the database.\n\n"
-               "  - siddb --help\n"
-               "    Prints help.\n\n"
-               "  - siddb --clean\n"
-               "    Cleans the database.\n\n"
-               "  - siddb <string ID>\n"
-               "    Looks up corresponding string of provided string ID (hex or decimal).\n\n"
-               "  - siddb <string>\n"
-               "    Looks up corresponding string ID of provided string (unquoted).\n\n";
+               "  - --list\n"
+               "    Lists all SID-string pairs in the database.\n"
+               "  - --help\n"
+               "    Prints help.\n"
+               "  - --clean\n"
+               "    Cleans the database.\n"
+               "  - <string ID>\n"
+               "    Looks up corresponding string of provided string ID (hex or decimal).\n"
+               "  - <string>\n"
+               "    Looks up corresponding string ID of provided string (unquoted).\n";
 }
-
-static const char *s_databaseFilePath = "siddb.db";
-static const char *s_logFilePath = "siddb.log";
 
 int main(int argc, const char** argv)
 {
-  siddb::Config(std::cout, s_logFilePath);
-  if (!siddb::Load(s_databaseFilePath))
-    return -1;
+  siddb::Config(std::cout, "siddb.log");
+  siddb::Clean();
 
-  switch (argc)
+  bool shouldExit = false;
+  while (!shouldExit)
   {
-    case 2:
-      // option
-      if (std::strlen(argv[1]) > 2 && argv[1][0] == '-' && argv[1][1] == '-')
+    char buffer[siddb::kMaxStrLen + 1];
+    std::printf("sidbot>");
+    scanf_s("%s", buffer, sizeof(buffer));
+
+    // option
+    if (std::strlen(buffer) > 2 && buffer[0] == '-' && buffer[1] == '-')
+    {
+      const char* pOption = buffer + 2;
+
+      // list
+      if (!std::strcmp(pOption, "list"))
       {
-        const char* pOption = argv[1] + 2;
-        if (!std::strcmp(pOption, "list"))
-        {
-          StringId::Storage numEntries = siddb::GetSize();
+        StringId::Storage numEntries = siddb::GetSize();
+        std::printf("  Number of entries: %lld\n", numEntries);
 
-          std::printf("Number of entries: %lld\n", numEntries);
-
-          siddb::ForEach
-          (
+        siddb::ForEach
+        (
             [](StringId sid, const char* str) -> bool
             {
-              std::printf("0x%016llx <-> %s\n", sid.GetValue(), str);
+              std::printf("    0x%016llx <-> %s\n", sid.GetValue(), str);
               return true;
             }
-          );
-        }
-        else if (!std::strcmp(pOption, "clean"))
-        {
-          siddb::Clean();
-
-          std::printf("SID database cleaned.\n");
-        }
-        else /* if (!std::strcmp(pOption, "help")) */
-        {
-          PrintUsage();
-        }
+        );
       }
-      // hex
-      else if (argv[1][0] == '0' && (argv[1][1] == 'x' || argv[1][1] == 'X'))
+      // clean
+      else if (!std::strcmp(pOption, "clean"))
       {
-        StringId::Storage sidVal;
-        sscanf_s(argv[1], "%llx", &sidVal);
-
-        char str[siddb::kMaxStrLen];
-
-        if (siddb::Find(StringId(sidVal), str))
+        siddb::Clean();
+        std::printf("  SID database cleaned.\n");
+      }
+      // save
+      else if (!std::strcmp(pOption, "save"))
+      {
+        scanf_s("%s", buffer, sizeof(buffer));
+        if (siddb::Save(buffer))
         {
-          std::printf("0x%016llx -> %s\n", sidVal, str);
+          std::printf("  SID database saved: %s\n", buffer);
         }
         else
         {
-          std::printf("0x%016llx -> ???\n", sidVal);
+          std::printf("  Cannot save SID database: %s\n", buffer);
         }
       }
-      // dec
-      else if (argv[1][0] >= '0' && argv[1][0] <= '9')
+      // load
+      else if (!std::strcmp(pOption, "load"))
       {
-        StringId::Storage sidVal;
-        sscanf_s(argv[1], "%lld", &sidVal);
-
-        char str[siddb::kMaxStrLen];
-        if (siddb::Find(StringId(sidVal), str))
+        scanf_s("%s", buffer, sizeof(buffer));
+        if (siddb::Load(buffer))
         {
-          std::printf("0x%016llx -> %s\n", sidVal, str);
+          std::printf("  SID database loaded: %s\n", buffer);
         }
         else
         {
-          std::printf("0x%016llx -> ???\n", sidVal);
+          std::printf("  Cannot load SID database: %s\n", buffer);
         }
       }
-      // string
+      // delete
+      else if (!std::strcmp(pOption, "delete"))
+      {
+        scanf_s("%s", buffer, sizeof(buffer));
+        if (siddb::Delete(buffer))
+        {
+          std::printf("  SID database deleted: %s\n", buffer);
+        }
+        else
+        {
+          std::printf("  Cannot delete SID database: %s\n", buffer);
+        }
+      }
+      // help
+      else /* if (!std::strcmp(pOption, "help")) */
+      {
+        PrintUsage();
+      }
+    }
+    // hex
+    else if (buffer[0] == '0' && (buffer[1] == 'x' || buffer[1] == 'X'))
+    {
+      StringId::Storage sidVal;
+      sscanf_s(buffer, "%llx", &sidVal);
+
+      char str[siddb::kMaxStrLen];
+
+      if (siddb::Find(StringId(sidVal), str))
+      {
+        std::printf("  0x%016llx -> %s\n", sidVal, str);
+      }
       else
       {
-        StringId sid;
-        if (siddb::Find(argv[1], &sid))
-        {
-          std::printf("%s -> 0x%016llx\n", argv[1], sid.GetValue());
-        }
-        else
-        {
-          siddb::Add(argv[1]);
-          std::printf("New entry saved:\n%s -> 0x%016llx\n", argv[1], SID_VAL(argv[1]));
-        }
+        std::printf("  0x%016llx -> ???\n", sidVal);
       }
-      break;
+    }
+    // dec
+    else if (buffer[0] >= '0' && buffer[0] <= '9')
+    {
+      StringId::Storage sidVal;
+      sscanf_s(buffer, "%lld", &sidVal);
 
-    default:
-      PrintUsage();
-      break;
+      char str[siddb::kMaxStrLen];
+      if (siddb::Find(StringId(sidVal), str))
+      {
+        std::printf("  0x%016llx -> %s\n", sidVal, str);
+      }
+      else
+      {
+        std::printf("  0x%016llx -> ???\n", sidVal);
+      }
+    }
+    // string
+    else
+    {
+      StringId sid;
+      if (siddb::Find(buffer, &sid))
+      {
+        std::printf("  %s -> 0x%016llx\n", buffer, sid.GetValue());
+      }
+      else
+      {
+        siddb::Add(buffer);
+        std::printf("  New entry saved:\n  %s -> 0x%016llx\n", buffer, SID_VAL(buffer));
+      }
+    }
   }
-
-  siddb::Save(s_databaseFilePath);
 
   return 0;
 }
