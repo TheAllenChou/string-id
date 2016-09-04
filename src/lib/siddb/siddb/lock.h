1ee3fcfdd523e2cae90add189ccdfff710108526
Allen Chou <cjcat2266@gmail.com>
2016-09-04 01:05:25 -0700

    RwLock.

--- src/lib/siddb/siddb/lock.h
+++ src/lib/siddb/siddb/lock.h
@@ -25,32 +25,42 @@ namespace siddb
     private:
       void AcquireReadLock()
       {
-        
+        std::unique_lock<std::mutex> lock(m_counterMutex);
+        while (!ReadLockPredicate())
+          m_cv.wait(lock);
+        ++m_counter;
       }
 
       void ReleaseReadLock()
       {
-        
+        std::unique_lock<std::mutex> lock(m_counterMutex);
+        --m_counter;
+        m_cv.notify_one();
       }
 
       void AcquireWriteLock()
       {
-        
+        std::unique_lock<std::mutex> lock(m_counterMutex);
+        m_counter |= kWriterFlag;
+        while (!WriteLockPredicate())
+          m_cv.wait(lock);
       }
 
       void ReleaseWriteLock()
       {
-        
+        std::unique_lock<std::mutex> lock(m_counterMutex);
+        m_counter &= ~kWriterFlag;
+        m_cv.notify_all();
       }
 
       bool ReadLockPredicate()
       {
-        
+        return !(m_counter & kWriterFlag) && m_counter < kMaxReaders;
       }
 
       bool WriteLockPredicate()
       {
-        
+        return m_counter == kWriterFlag;
       }
 
       static const size_t kWriterFlag = size_t(1) <<  (sizeof(size_t) * 8 - 1);
